3da931970fd7a69e5d85fc21fd1f4856b7b2c03c
June Park <rjp2030@outlook.com>
2023-04-08 11:29:10 +0900

    remove cython

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
 setup(
+    packages=find_packages(),
     name='getprimes',
-    ext_modules=cythonize("getprimes.py"),
     zip_safe=False,
-    install_requires=['Cython'],
 )
 
